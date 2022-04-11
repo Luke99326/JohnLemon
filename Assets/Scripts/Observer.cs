94dbaa93b2661b133627cbf8a50d70a3575d0607
luke99326 <lukegscribner@gmail.com>
2022-04-11 14:43:53 -0700

    finished particles and lerp

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class Observer : MonoBehaviour
 {
     public Transform player;
+    public Transform ghost;
     public GameEnding gameEnding;
+    public ParticleSystem ps;
 
     bool m_IsPlayerInRange;
+
+    private void Start()
+    {
+        var main = ps.main;
+        main.startColor = new Color(.54f, .71f, .80f, 1f);
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -27,8 +35,27 @@ public class Observer : MonoBehaviour
 
     void Update()
     {
+
+        
+        float dist = Vector3.Distance(player.transform.position, ghost.transform.position);
+        Vector3 lerp = Vector3.Lerp(player.position, ghost.position, dist);
+
+        var main = ps.main;
+        main.startColor = new Color(1.5f - Mathf.Abs(lerp.z / 3), .71f, .80f, 1f);
+        if (ghost.name == "Ghost")
+        {
+            print("lerp: " + (1.5 - Mathf.Abs(lerp.z / 3) + ghost.name));
+            print("dist: " + dist);
+
+        }
+
+
         if (m_IsPlayerInRange)
         {
+
+            print("player:" + player.position);
+            print("ghost:" + ghost.position);
+
             Vector3 direction = player.position - transform.position + Vector3.up;
             Ray ray = new Ray(transform.position, direction);
             RaycastHit raycastHit;
